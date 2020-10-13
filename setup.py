fe4f892241d25501c1f2383eecc4a9a194219f7a
Juan Manuel Ruiz Fernández <jruizferprofesional@gmail.com>
2020-10-13 21:09:32 +0200

    [workflows] Fix invalid publish command
    Fixed setup final version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="tflens",
-  version="0.1.0",
+  version="1.0.0",
   author="Juan Manuel Ruiz Fernández",
   description="Terraform state viewer",
   keywords="terraform viewer state tfstate cli",
